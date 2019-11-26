58d515f66c4725dc6c782ddbe142fc39b1f9ef26
Grondag <grondag@gmail.com>
2019-11-25 20:21:26 -0800

    Update  for 1.15-pr2

---
+++ build.sh
@@ -0,0 +1,36 @@
+cd builds
+
+cd parent
+echo "fermion parent"
+../../gradlew $1 $2
+cd ..
+
+echo "fermion-varia"
+cd varia
+../../gradlew $1 $2
+cd ..
+
+echo "special-circumstances"
+cd special-circumstances
+../../gradlew $1 $2
+cd ..
+
+echo "fermion-simulator"
+cd simulator
+../../gradlew $1 $2
+cd ..
+
+echo "fermion-gui"
+cd gui
+../../gradlew $1 $2
+cd ..
+
+echo "fermion-modkeys"
+cd modkeys
+../../gradlew $1 $2
+cd ..
+
+cd ..
+
+echo "fermion bundle"
+./gradlew $1 $2
