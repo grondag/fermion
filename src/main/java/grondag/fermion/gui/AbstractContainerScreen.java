8d9972e30896617cac0ebf47a4837f2610e441f3
Grondag <grondag@gmail.com>
2019-12-17 06:39:02 -0800

    Gui Rafactors

--- src/main/java/grondag/fermion/gui/AbstractContainerScreen.java
+++ src/main/java/grondag/fermion/gui/AbstractContainerScreen.java
@@ -4,52 +4,54 @@ import net.minecraft.container.Container;
 import net.minecraft.container.Slot;
 import net.minecraft.text.Text;
 
-import grondag.fermion.gui.control.AbstractControl;
+import net.fabricmc.loader.api.FabricLoader;
 
 public abstract class AbstractContainerScreen extends AbstractSimpleScreen
 {
-	protected int xSize;
-
-	protected int ySize;
-
-	protected int guiLeft;
-	protected int guiRight;
-	protected int guiTop;
-
 	protected Container inventorySlots;
 	protected final ContainerLayout layout;
-
-	public static final ContainerLayout LAYOUT;
-
-	static {
-		LAYOUT = new ContainerLayout();
-
-		LAYOUT.dialogHeight = LAYOUT.externalMargin * 3 + LAYOUT.slotSpacing * 4 + LAYOUT.playerInventoryWidth + AbstractControl.CONTROL_INTERNAL_MARGIN;
-
-		/** distance from top of dialog to start of player inventory area */
-		LAYOUT.playerInventoryTop = LAYOUT.dialogHeight - LAYOUT.externalMargin - LAYOUT.slotSpacing * 4 - AbstractControl.CONTROL_INTERNAL_MARGIN;
-	}
 
 	public AbstractContainerScreen(ContainerLayout layout, Container container, Text title) {
 		super(title);
 		inventorySlots = container;
 		this.layout = layout;
-		xSize = layout.dialogWidth;
-		ySize = layout.dialogHeight;
+	}
+
+	@Override
+	public void init() {
+		super.init();
+	}
+
+	@Override
+	protected void computeScreenBounds() {
+		super.computeScreenBounds();
+		screenWidth = layout.dialogWidth;
+		screenHeight = layout.dialogHeight;
+
+		screenTop = (height - screenHeight) / 2;
+
+		// if using REI, center on left 2/3 of screen to allow more room for REI
+		if(FabricLoader.getInstance().isModLoaded("rei")) {
+			screenLeft = ((width * 2 / 3) - screenWidth) / 2;
+		} else {
+			screenLeft = (width - screenWidth) / 2;
+		}
 	}
 
 	@Override
 	public void renderBackground() {
 		super.renderBackground();
 
-		GuiUtil.drawRect(guiLeft, guiTop, guiLeft + xSize, guiTop + ySize, 0xFFCCCCCC);
-
 		// player slot backgrounds
-		for(final Slot slot : inventorySlots.slotList)
-		{
-			final int x = guiLeft + slot.xPosition;
-			final int y = guiTop + slot.yPosition;
-			GuiUtil.drawGradientRect(x, y, x + 16, y + 16, 0xFFA9A9A9, 0xFF898989);
+		for(final Slot slot : inventorySlots.slotList) {
+			final int x = screenLeft + slot.xPosition;
+			final int y = screenTop + slot.yPosition;
+			fillGradient(x, y, x + 16, y + 16, 0xFFA9A9A9, 0xFF898989);
 		}
+	}
+
+	@Override
+	protected void drawControls(int mouseX, int mouseY, float partialTicks) {
+		super.drawControls(mouseX, mouseY, partialTicks);
 	}
 }
--- src/main/java/grondag/fermion/gui/AbstractSimpleScreen.java
+++ src/main/java/grondag/fermion/gui/AbstractSimpleScreen.java
@@ -50,13 +50,20 @@ public abstract class AbstractSimpleScreen extends Screen implements ScreenRende
 	@Override
 	public void init() {
 		super.init();
+		computeScreenBounds();
+		mainPanel = createMainPanel();
+		children.add(mainPanel);
+		addControls(mainPanel);
+	}
+
+	/**
+	 * Called during init before controls are created.
+	 */
+	protected void computeScreenBounds() {
 		screenHeight = height * 4 / 5;
 		screenTop = (height - screenHeight) / 2;
 		screenWidth = (int) (screenHeight * GuiUtil.GOLDEN_RATIO);
 		screenLeft = (width - screenWidth) / 2;
-		mainPanel = createMainPanel();
-		children.add(mainPanel);
-		addControls(mainPanel);
 	}
 
 	@Override
@@ -65,31 +72,33 @@ public abstract class AbstractSimpleScreen extends Screen implements ScreenRende
 	}
 
 	@Override
-	public void render(int mouseX, int mouseY, float partialTicks) {
+	public void renderBackground() {
+		super.renderBackground();
+		fill(screenLeft, screenTop, screenLeft + screenWidth, screenTop + screenHeight, 0xFFCCCCCC);
+	}
+
+	@Override
+	public final void render(int mouseX, int mouseY, float partialTicks) {
 		// TODO: make generic
 		// ensure we get updates
 		//te.notifyServerPlayerWatching();
 
 		hoverControl = null;
-		super.renderBackground();
 
-		fill(screenLeft, screenTop, screenLeft + screenWidth, screenTop + screenHeight, 0xFFCCCCCC);
+		renderBackground();
 
 		// shouldn't do anything but call in case someone is hooking it
 		super.render(mouseX, mouseY, partialTicks);
 
-		// Draw controls here because foreground layer is translated to frame of the GUI
-		// and our controls are designed to render in frame of the screen.
-		// And can't draw after super.drawScreen() because would potentially render on
-		// top of things.
-
-		//MachineControlRenderer.setupMachineRendering();
-		mainPanel.drawControl(mouseX, mouseY, partialTicks);
-		//MachineControlRenderer.restoreGUIRendering();
+		drawControls(mouseX, mouseY, partialTicks);
 
 		if (hoverControl != null) {
 			hoverControl.drawToolTip(mouseX, mouseY, partialTicks);
 		}
+	}
+
+	protected void drawControls(int mouseX, int mouseY, float partialTicks) {
+		mainPanel.drawControl(mouseX, mouseY, partialTicks);
 	}
 
 	@Override
