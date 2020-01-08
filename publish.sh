4fd11e00341d46b23a14d2c5c39977af4a2b3d96
Grondag <grondag@gmail.com>
2020-01-07 19:37:31 -0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,33 +1,32 @@
-cd builds
 
-cd parent
 echo "fermion parent"
-../../gradlew $1 $2
-cd ..
+./gradlew publish
+
+cd builds
 
 echo "fermion-varia"
 cd varia
-../../gradlew $1 $2
+../../gradlew publish
 cd ..
 
 echo "special-circumstances"
 cd special-circumstances
-../../gradlew $1 $2
+../../gradlew publish
 cd ..
 
 echo "fermion-simulator"
 cd simulator
-../../gradlew $1 $2
+../../gradlew publish
 cd ..
 
 echo "fermion-gui"
 cd gui
-../../gradlew $1 $2
+../../gradlew publish
 cd ..
 
 echo "fermion-modkeys"
 cd modkeys
-../../gradlew $1 $2
+../../gradlew publish
 cd ..
 
 cd ..
